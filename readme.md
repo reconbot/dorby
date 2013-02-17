c8e0bf52a2a99b11b7e06a50bd86a795991e5793
Francis Gulotta <wizard@roborooter.com>
2013-02-17 15:37:58 -0500

    basic user interface

---
+++ readme.md
@@ -0,0 +1,6 @@
+DOOR
+===
+
+This program will open my door.
+
+Please don't use it.
--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 /*global require:true */
 var arduino = require('./arduino');
-// arduino.openDoor();
+arduino.openDoor();
 // arduino.buildStatus(false);
+
+var express = require('express');
+var app = express();
+
+app
+  .use(express.logger('dev'))
+  .use(express['static']('public'))
+  .use(function (req, res, next) {
+    if (req.url === '/openDoor') {
+      arduino.openDoor();
+      res.redirect('/success.html');
+      return;
+    }
+    next();
+  })
+  .listen(3000);
